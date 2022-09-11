4a642db29cce84faa27c6c6fbab5604284a3aea3
Manuel Schröder <manuel.schroeder@storyblok.com>
2022-09-11 19:13:50 +0200

    update

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,15 +1,22 @@
 module.exports = {
   content: ['./src/**/*.{astro,html,js,md,vue}'],
+  darkMode: 'class',
   theme: {
     container: {
       center: true,
       padding: '2rem',
     },
+    fontFamily: {
+      sans: 'Inter, sans-serif',
+    },
     extend: {
       colors: {
-        primary: '#86354E',
+        //primary: '#86354E',
+        primary: '#b18854',
+        primary_alt: '#86354E',
         secondary: '#1c243d',
       },
     },
   },
+  plugins: [require('@tailwindcss/typography'), require('@tailwindcss/line-clamp')],
 }
