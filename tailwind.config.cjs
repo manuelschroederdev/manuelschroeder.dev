03d9bd3780a85d3ca2f52c6905c8ce501dab6e03
Manuel Schröder <manuel.schroeder@storyblok.com>
2022-09-25 19:17:56 +0200

    add code highlighting

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,3 +1,13 @@
+const disabledCss = {
+  'code::before': false,
+  'code::after': false,
+  'blockquote p:first-of-type::before': false,
+  'blockquote p:last-of-type::after': false,
+  pre: false,
+  code: false,
+  'pre code': false,
+}
+
 module.exports = {
   content: ['./src/**/*.{astro,html,js,md,vue}'],
   darkMode: 'class',
@@ -13,6 +23,13 @@ module.exports = {
       colors: {
         primary: '#b18854',
         primary_alt: '#86354E',
+      },
+      typography: {
+        DEFAULT: { css: disabledCss },
+        sm: { css: disabledCss },
+        lg: { css: disabledCss },
+        xl: { css: disabledCss },
+        '2xl': { css: disabledCss },
       },
     },
   },
