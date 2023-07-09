67c2cf3ca773820130b29ca486a0083637123aa2
Manuel Schröder <manuel.schroeder@storyblok.com>
2023-07-09 12:51:26 +0200

    update deps

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -33,8 +33,5 @@ module.exports = {
       },
     },
   },
-  plugins: [
-    require('@tailwindcss/typography'),
-    require('@tailwindcss/line-clamp'),
-  ],
+  plugins: [require('@tailwindcss/typography')],
 }
